6184f052e2fbb985746d582d46bf041ef947c205
T G BALASUBRAMANIAM <157718312+TheTechieGeek@users.noreply.github.com>
2026-03-10 01:33:36 +0530

    Update block_bcd.sv

--- BCD ADDER/block_bcd.sv
+++ BCD ADDER/block_bcd.sv
@@ -1,11 +1,25 @@
-module pg_block(
-    input a,
-    input b,
-    output p,
-    output g
+module BCD_digit(
+    input  logic [3:0] a,
+    input  logic [3:0] b,
+    input  logic       cin,
+    output logic [3:0] sum,
+    output logic       cout
 );
-    
-assign p = a ^ b;
-assign g = a & b;
+
+    logic [4:0] temp;
+
+    always_comb begin
+        temp = a + b + cin;
+
+        if (temp > 9) begin
+            temp = temp + 5'd6;
+            cout = 1;
+        end
+        else begin
+            cout = 0;
+        end
+
+        sum = temp[3:0];
+    end
 
 endmodule
